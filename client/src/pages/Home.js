8fe4c10422794831a17d93e160c56cfb39e9c549
“SuleRas” <sm.ras@hotmailco.uk>
2022-11-28 20:47:41 +0000

    (home.js) add which component u want to render

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
+
+const Home = () => {
+  return (
+    <div className="container">
+      {/* to add what components you want to render */}
+      {/* <CategoryMenu />
+      <ProductList />
+      <Cart /> */}
+    </div>
+  );
+};
+
+export default Home;
