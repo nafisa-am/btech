ca5bf5e6a244cae6f5a6694b47c16942bd5a0466
EthanDMello <107923914+EthanDMello@users.noreply.github.com>
2022-11-28 18:02:02 +0000

    changing from react bootstrap to emotion

--- client/src/components/signUp.js
+++ client/src/components/signUp.js
@@ -67,37 +67,37 @@ const SignupForm = () => {
           Something went wrong with your signup!
         </Alert>
 
-        <Form.Group>
-          <Form.Label htmlFor="username">Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your username"
-            name="username"
-            onChange={handleInputChange}
-            value={userFormData.username}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Username is required!
-          </Form.Control.Feedback>
-        </Form.Group>
+        <form>
+          <Form.Group>
+            <Form.Label htmlFor="username">Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your username"
+              name="username"
+              onChange={handleInputChange}
+              value={userFormData.username}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Username is required!
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group>
-          <Form.Label htmlFor="email">Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Your email address"
-            name="email"
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Email is required!
-          </Form.Control.Feedback>
-        </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Your email address"
+              name="email"
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Email is required!
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <Form.Group>
           <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control
             type="password"
@@ -110,8 +110,9 @@ const SignupForm = () => {
           <Form.Control.Feedback type="invalid">
             Password is required!
           </Form.Control.Feedback>
-        </Form.Group>
-        <Button
+        </form>
+
+        <button
           disabled={
             !(
               userFormData.username &&
@@ -123,7 +124,7 @@ const SignupForm = () => {
           variant="success"
         >
           Submit
-        </Button>
+        </button>
       </Form>
     </>
   );
