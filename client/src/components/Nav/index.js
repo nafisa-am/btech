b9d362c96bfc7d872635af0257f31c747aa5ef6a
Ayan Ahmed <ayanahmed@ayans-mbp.home>
2022-12-07 13:41:23 +0000

    fixed some errors

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -13,8 +13,15 @@ function Nav() {
 				<div className="logo">
 					<img style={{ width: "200px", height: "200px" }} src={BtechLogo} />
 				</div>
+				<div class="collapse" id="navbarToggleExternalContent">
+					<div class="bg-dark p-4">
+						<h5 class="text-white h4">Collapsed content</h5>
+						<span class="text-muted">Toggleable via the navbar brand.</span>
+					</div>
+				</div>
+
 				{/* Search bar */}
-				<div class="container-fluid">
+				<div class="container-search">
 					<form class="d-flex input-group w-auto">
 						<input
 							type="search"
@@ -25,54 +32,7 @@ function Nav() {
 						/>
 					</form>
 				</div>
-				{/* User Icons */}
-				<div class="container-user">
-					<ul class="navbar-nav d-flex flex-row">
-						<li class="nav-item me-3 me-lg-0">
-							<a class="nav-link" href="#">
-								<i class="fas fa-shopping-cart"></i>
-							</a>
-						</li>
-						<li class="nav-item me-3 me-lg-0">
-							<a class="nav-link" href="#">
-								<i class="fab fa-twitter"></i>
-							</a>
-						</li>
 
-						<li class="nav-item me-3 me-lg-0 dropdown">
-							<a
-								class="nav-link dropdown-toggle"
-								href="#"
-								id="navbarDropdown"
-								role="button"
-								data-mdb-toggle="dropdown"
-								aria-expanded="false"
-							>
-								<i class="fas fa-user"></i>
-							</a>
-							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<li>
-									<a class="dropdown-item" href="#">
-										Action
-									</a>
-								</li>
-								<li>
-									<a class="dropdown-item" href="#">
-										Another action
-									</a>
-								</li>
-								<li>
-									<hr class="dropdown-divider" />
-								</li>
-								<li>
-									<a class="dropdown-item" href="#">
-										Something else here
-									</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
 				{/* Navigation links */}
 				<ul>
 					<li className="mx-1">
@@ -122,21 +82,54 @@ function Nav() {
 							Order History
 						</Link>
 					</li>
+
+					{/* User Icons */}
+					<div class="container-user">
+						<ul class="navbar-nav d-flex flex-row">
+							<li class="nav-item me-3 me-lg-0">
+								<a class="nav-link" href="/cart">
+									<i class="fas fa-shopping-cart"></i>
+								</a>
+							</li>
+							<li class="nav-item me-3 me-lg-0">
+								<a class="nav-link" href="/twitter">
+									<i class="fab fa-twitter"></i>
+								</a>
+							</li>
+
+							<li class="nav-item me-3 me-lg-0 dropdown">
+								<li className="mx-1">
+									<i class="fas fa-user"></i>
+
+									<div className="dropdown">
+										<div
+											className="hyperlink dropdown-toggle"
+											type="button"
+											data-bs-toggle="dropdown"
+											aria-expanded="false"
+										></div>
+										<ul className="dropdown-menu" id="user">
+											<li>
+												<a className="dropdown-item" href="/my-account">
+													My Account
+												</a>
+											</li>
+										</ul>
+									</div>
+								</li>
+							</li>
+						</ul>
+					</div>
+
 					<li className="mx-1">
 						{/* this is not using the Link component to logout or user and then refresh the application to the start */}
-						<div
-							className="hyperlink"
-							onClick={
-								{
-									/*() => Auth.logout()*/
-								}
-							}
-						>
-							Logout
-						</div>
+						{/* <li className="mx-1">
+							<Link className="hyperlink" to="/logout">
+								Logout
+							</Link>
+						</li> */}
 					</li>
 				</ul>
-				{/* <ul className="flex-row"></ul> */}
 			</div>
 		</BrowserRouter>
 	);
