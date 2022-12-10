e9a512b15f7ee8880784000ab80fe757c08b58c7
Darren <you@example.comdarren@kandekore.net>
2022-12-10 00:22:46 +0000

    nav & home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 // import Data from "../mock-data.json";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Router>
+        {/* HEADER */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="loginSignup" element={<LoginSignup />} /> */}
+        </Routes>
+        {/* FOOTER */}
+      </Router>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,24 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+// import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+// import Cart from "../components/Cart";
 
 const Home = () => {
-	return (
-		<div className="container">
-			{/* to add what components you want to render */}
-			{/* <CategoryMenu />
+  return (
+    <div className="container">
+      {/* to add what components you want to render */}
+      {/* <CategoryMenu />
       <ProductList />
       <Cart /> */}
-			{Data.map((post) => (
-				<div key={post.id}>
-					<p>{post.product}</p>
-					<p>{post.category}</p>
-				</div>
-			))}
-		</div>
-	);
+      {/* {Data.map((post) => (
+        <div key={post.id}>
+          <p>{post.product}</p>
+          <p>{post.category}</p>
+        </div>
+      ))} */}
+      <h1>Home</h1>
+    </div>
+  );
 };
 
 export default Home;
