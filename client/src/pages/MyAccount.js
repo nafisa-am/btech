bf5f8e6a0ccc64d3894debe9f87e5790502a946e
Ayan Ahmed <ayanahmed@Ayans-MacBook-Pro.local>
2022-12-12 19:44:34 +0000

    started working on profile page

--- client/src/pages/MyAccount.js
+++ client/src/pages/MyAccount.js
@@ -1,10 +1,10 @@
 import React from "react";
 // import Auth from "../../utils/auth";
 import { Link, BrowserRouter } from "react-router-dom";
-import BtechLogo from "../../assets/logo2.png";
-import "./index.css";
+import BtechLogo from "../assets/logo2.png";
+import "../index.css";
 
-function Nav() {
+function Account() {
 	// if (Auth.loggedIn()) {
 	return (
 		<BrowserRouter>
@@ -110,13 +110,10 @@ function Nav() {
 										></div>
 										<ul className="dropdown-menu" id="user">
 											<li>
-												<a className="dropdown-item" href="/my-account">
+												<Link className="dropdown-item" to="/my-account">
 													My Account
-												</a>
+												</Link>
 											</li>
-											{/* <Link className="dropdown-item" to="/my-account">
-												My Account
-											</Link> */}
 										</ul>
 									</div>
 								</ul>
@@ -136,31 +133,6 @@ function Nav() {
 			</div>
 		</BrowserRouter>
 	);
-	// } else {
-	// 	return (
-	// 		<ul className="flex-row">
-	// 			<li className="mx-1">
-	// 				<Link to="/signup">Signup</Link>
-	// 			</li>
-	// 			<li className="mx-1">
-	// 				<Link to="/login">Login</Link>
-	// 			</li>
-	// 		</ul>
-	// 	);
-	// }
-
-	// return (
-	// 	<header className="flex-row px-1">
-	// 		<h1>
-	// 			<Link to="/">
-	// 				<span role="img" aria-label=""></span>
-	// 				BTECH
-	// 			</Link>
-	// 		</h1>
-
-	// 		<nav>{showNavigation()}</nav>
-	// 	</header>
-	// );
 }
 
-export default Nav;
+export default Account;
