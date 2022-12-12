9c967cdaaf1d0ce99fd8c2b9e4b0fafd82820e58
“SuleRas” <sm.ras@hotmailco.uk>
2022-12-12 23:40:20 +0000

    Removed array from SearchBar function

--- client/src/pages/SearchResults/index.js
+++ client/src/pages/SearchResults/index.js
@@ -1,60 +1,19 @@
-import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import BrandComponent from "../../components/filter/Brands";
 
 const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState("");
-
-  const products = [
-    { name: "Macbook", product: "Laptop" },
-    { name: "Acer", product: "Laptop" },
-    { name: "HP", product: "Laptop" },
-    { name: "Dell", product: "Laptop" },
-    { name: "Lenovo", product: "Laptop" },
-    { name: "Iphone", product: "Mobile" },
-    { name: "Samsung", product: "Mobile" },
-    { name: "Huawei", product: "Mobile" },
-    { name: "Xiaomi", product: "Mobile" },
-    { name: "Apple", product: "Tablet" },
-    { name: "Samsung", product: "Tablet" },
-    { name: "Huawei", product: "Tablet" },
-  ];
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-  };
-
-  if (searchInput.length > 0) {
-    products.filter((product) => {
-      return product.name.match(searchInput);
-    });
-  }
-
   return (
-    <div>
-      <input
+    <div className="d-flex justify-content-between flex-column flex-md-row ">
+      {/* <ProductType /> */}
+      <BrandComponent />
+
+      {/* <input
         type="search"
         placeholder="Search here"
         onChange={handleChange}
         value={searchInput}
-      />
-
-      <table>
-        <tbody>
-          <tr>
-            <th>Models</th>
-            <th>product</th>
-          </tr>
-
-          {products.map((product) => {
-            return (
-              <tr key={`${product.name}-${product.product}`}>
-                <td>{product.name}</td>
-                <td>{product.product}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      /> */}
     </div>
   );
 };
